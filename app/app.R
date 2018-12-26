dbcbcff9d77960b23022677359541c15ca1ada42
Jutong Pan <panjutong@gmail.com>
2018-12-25 21:33:26 -0500

    app.R: add Types selector and Awoken Skills selector

--- app/app.R
+++ app/app.R
@@ -9,22 +9,46 @@ ui <- fluidPage (
   wellPanel(
     h3("Filters"),
     prettyRadioButtons(
-      inputId = "MainAtt",
+      inputId = "selectMainAtt",
       label = "Main Attribute",
       choices = c("Fire", "Water", "Wood", "Light", "Dark", "Any"),
+      selected = "Any",
       inline = T
     ),
     prettyRadioButtons(
-      inputId = "SubAtt",
+      inputId = "selectSubAtt",
       label = "Sub Attribute",
       choices = c("Fire", "Water", "Wood", "Light", "Dark", "Null", "Any"),
+      selected = "Any",
       inline = T
     ),
-    awesomeCheckboxGroup(
-      inputId = "Type",
+    prettyCheckboxGroup(
+      inputId = "selectType",
       label = "Type",
-      choices = c("God", "Devil", "Dragon", "Machine", "Physical", "Healer", "Killer"),
-      inline = T
+      choices = c("God", "Devil", "Dragon", "Balance", "Physical", "Killer", "Healer",
+        "Machine", "Evolve", "Enhance", "Awoken", "Vendor"),
+      inline = T,
+      status = "primary"
+    ),
+    tags$b("Awoken Skills"),
+    div(
+      style = "display: flex; flex-wrap: wrap",
+      uiOutput(
+        outputId = "selectedAwokenSkills"
+      ),
+      div(
+        style = "padding-left:10px",
+        actionButton(
+          inputId = "clearSelectedAwokenSkills",
+          label = "clear"
+        )
+      )
+    ),
+    checkboxGroupButtons(
+      inputId = "selectAwokenSkills",
+      label = "",
+      choices = "",
+      individual = T
     )
   )
 
@@ -32,6 +56,67 @@ ui <- fluidPage (
 
 server <- function(input, output, session) {
 
+  AwokenSkill.dt <- data.table(
+    AwokenSkill = c(
+      "Enhance HP",
+      "Enhance ATK",
+      "Enhance RCV"
+    ),
+    AwokenSkillIconPath = c(
+      "http://www.puzzledragonx.com/en/img/awoken/3.png",
+      "http://www.puzzledragonx.com/en/img/awoken/4.png",
+      "http://www.puzzledragonx.com/en/img/awoken/5.png"
+    )
+  )
+
+  AwokenSkill.dt[, LinkHtml := paste0("<img src=", AwokenSkillIconPath, ">")]
+
+  getAwokenSkillChoices <- function(AwokenSkill.dt) {
+    choices <- AwokenSkill.dt$AwokenSkill
+    names(choices) <- AwokenSkill.dt$LinkHtml
+    choices
+  }
+
+  initSelectAwokenSkills <- function() {
+    updateCheckboxGroupButtons(
+      session = session,
+      inputId = "selectAwokenSkills",
+      choices = getAwokenSkillChoices(AwokenSkill.dt),
+      selected = NULL
+    )
+  }
+
+  initSelectAwokenSkills()
+
+  selectedAwokenSkills <- reactiveVal(NULL)
+
+  observeEvent(input$selectAwokenSkills, {
+
+    selectedAwokenSkills(
+      tagList(
+        selectedAwokenSkills(),
+        tags$img(src = AwokenSkill.dt[AwokenSkill == input$selectAwokenSkills, AwokenSkillIconPath])
+      )
+    )
+
+    output$selectedAwokenSkills <- renderUI(
+      selectedAwokenSkills()
+    )
+
+    initSelectAwokenSkills()
+
+  }, ignoreNULL = T, ignoreInit = T
+  )
+
+  observeEvent(input$clearSelectedAwokenSkills, {
+
+    selectedAwokenSkills(NULL)
+
+    output$selectedAwokenSkills <- renderUI(
+      selectedAwokenSkills()
+    )
+
+  })
 
 
 }
