37b537da7cdb1026dfd3323d8da0e1198e3f733f
Yawen Liang <neway.liang@gmail.com>
2018-12-26 22:25:24 -0500

    Added scraping files AwokenSkill.r and Attribute.r. Added Attribute images.

---
+++ padmonster_crawler/Attribute.R
@@ -0,0 +1,32 @@
+rm(list=ls())
+library(data.table)
+library(rvest)
+
+setwd("//Users/yawenliang/Documents/PadMonster")
+
+
+## 1. Scrape awoken skill data
+
+url <- 'http://pad.skyozora.com/pets/'
+webpage <- read_html(url)
+webnodes <- html_nodes(webpage, '#property1 label img')
+
+AttributeIconDownload <- sapply(webnodes, function(x){xml_attr(x,"src")})
+Attribute.dt <- data.table(AttributeIconDownload)
+Attribute.dt[ , Id := 1:nrow(Attribute.dt)]
+Attribute.dt[ , AttributeName := c("Fire", "Water", "Wood", "Light", "Dark")]
+
+for(i in 1:length(Attribute.dt$AttributeIconDownload)){
+  download.file(paste0("http://pad.skyozora.com/",Attribute.dt$AttributeIconDownload[i]),paste0("app/img/Attribute/", Attribute.dt[i,AttributeName], ".png"))
+}
+
+Attribute.dt[ ,AttributeIconPath := paste0("img/Attribute/", AttributeName,".png")]
+Attribute.dt[ ,AttributeIconDownload := NULL]
+
+setcolorder(Attribute.dt, c("Id", "AttributeName", "AttributeIconPath"))
+
+## 2. Write the data into database
+conn <- dbConnect(drv = RSQLite::SQLite(), "db/padmonster.sqlite3")
+dbWriteTable(conn, "Attribute", Attribute.dt, overwrite = TRUE)
+dbDisconnect(conn) 
+
---
+++ padmonster_crawler/AwokenSkill.R
@@ -0,0 +1,48 @@
+rm(list=ls())
+library(data.table)
+library(rvest)
+library(DBI)
+library(RSQLite)
+
+setwd("//Users/yawenliang/Documents/PadMonster")
+
+## 1. Scrape awoken skill data
+
+url <- 'http://pad.skyozora.com/skill/%E8%A6%BA%E9%86%92%E6%8A%80%E8%83%BD%E4%B8%80%E8%A6%BD/'
+webpage <- read_html(url)
+webnodes <- html_nodes(webpage, '.tooltip')
+
+AwokenSkillName <- sapply(webnodes, function(x) xml_attr(x, "title"))
+AwokenSkillIconDownload <- sapply(webnodes, function(x) xml_attr(xml_child(x), "src"))
+
+AwokenSkillName.dt <- data.table(AwokenSkillName)
+AwokenSkillIconDownload.dt <- data.table(AwokenSkillIconDownload)
+
+AwokenSkill.dt <- cbind(AwokenSkillName.dt, AwokenSkillIconDownload.dt)
+AwokenSkill.dt <- AwokenSkill.dt[-1,]
+AwokenSkill.dt[ , AwokenSkillId := 1:nrow(AwokenSkill.dt)]
+
+
+for(i in 1:length(AwokenSkill.dt$AwokenSkillIconDownload)){
+download.file(paste0(AwokenSkill.dt$AwokenSkillIconDownload[i]),paste0("app/img/AwokenSkill/", AwokenSkill.dt[i,AwokenSkillId], ".png"))
+}
+
+AwokenSkill.dt[ ,AwokenSkillIconPath := paste0("img/AwokenSkill/", AwokenSkillId,".png")]
+
+
+## 2. Write the data into database
+conn <- dbConnect(drv = RSQLite::SQLite(), "db/padmonster.sqlite3")
+
+# a <- dbListTables(conn)
+# for (i in 1:length(a)) {
+#   assign(a[i], value = dbReadTable(conn, a[i]))
+# }
+# AwokenSkill <- dbReadTable(conn, "AwokenSkill")
+
+AwokenSkill.dt[, AwokenSkillIconDownload := NULL]
+AwokenSkill.dt[, AwokenSkillDescription := NA]
+AwokenSkill.dt[, Id:= 1:nrow(AwokenSkill.dt) ]
+setcolorder(AwokenSkill.dt, c("Id", "AwokenSkillId"))
+
+dbWriteTable(conn, "AwokenSkill", AwokenSkill.dt, overwrite = TRUE)
+dbDisconnect(conn) 
