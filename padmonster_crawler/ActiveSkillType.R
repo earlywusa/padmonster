39dba2fd43d74f1e9f8d89023771908051e53e79
Yawen Liang <neway.liang@gmail.com>
2018-12-31 11:37:19 -0500

    Modifying ActiveSkillType.R

--- padmonster_crawler/ActiveSkillType.R
+++ padmonster_crawler/ActiveSkillType.R
@@ -12,35 +12,32 @@ url <- 'http://pad.skyozora.com/skill/%E4%B8%BB%E5%8B%95%E6%8A%80%E8%83%BD%E4%B8
 webpage <- read_html(url)
 webnodes1 <- html_nodes(webpage, 'h3')
 webnodes2 <- html_nodes(webpage, 'table')
+
 ActiveSkillType.ls <- as.list(html_text(webnodes1))
-ActiveSkill.ls <- lapply(webnodes2, function(x) html_text(html_nodes(x, "tr td")))
-text3 <- text2[4:(length(text2)-1)]
-
-text3_upgrade <- lapply(text3, data.table)
-
-
-text1.dt <- data.table(text1)
-text3.dt <- data.table(text3)
-
-miao <- combine.lists(text1, text3)
+ActiveSkillType.dt <- data.table(ActiveSkillType.ls)
+ActiveSkillContent.ls_ <- lapply(webnodes2, function(x) html_text(html_nodes(x, "tr td")))
+ActiveSkillContent.ls_ <- ActiveSkillContent.ls_[4:(length(ActiveSkillContent.ls_ )-1)]
 
 
+wide <- function(x){
+  name_index <- seq(from = 1, to = length(x)-1, by = 2)
+  description_index <- name_index+1
+  name <- x[name_index]
+  description <- x[description_index]
+  data.table(ActiveSkillName = name, ActiveSkillDescription = description)
+}
+ActiveSkillContent.ls <- lapply(ActiveSkillContent.ls_, wide)
 
 
+ActiveSkillType.ls <- list()
+for (i in 1:length(ActiveSkillType.ls)){
+  ActiveSkillType.ls[[i]] <- ActiveSkillContent.ls[[i]][, ActiveSkillType := ActiveSkillType.ls[[i]]]
+}
+
+ActiveSkillType.dt1 <- rbindlist(ActiveSkillType.ls)
 
 
-url <- 'http://pad.skyozora.com/pets/'
-webpage <- read_html(url)
-webnodes <- html_nodes(webpage, '#skill option')
+# Get ActiveSkillId
 
-
-
-ActiveSkillType <- sapply(webnodes, function(x) xml_attr(x, "value"))
-ActiveSkillType.dt <- data.table(ActiveSkillType)
-ActiveSkillType.dt <- ActiveSkillType.dt[2:nrow(ActiveSkillType.dt), ]
-
-# Categorize active skills
 conn <- dbConnect(drv = RSQLite::SQLite(), "db/padmonster.sqlite3")
-ActiveSkill <- dbReadTable(conn, "ActiveSkill")
-
-# Write into database
+miao <- dbReadTable(conn,"ActiveSkill")
