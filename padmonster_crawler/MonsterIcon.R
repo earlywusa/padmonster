8c6b9b1d4b7d81875bbb11d371013b3a3d52bd31
Jutong Pan <panjutong@gmail.com>
2018-12-30 23:36:18 -0500

    MonsterIcon.R: download iff file not exists

--- padmonster_crawler/MonsterIcon.R
+++ padmonster_crawler/MonsterIcon.R
@@ -1,13 +1,16 @@
 rm(list=ls())
 library(data.table)
+library(DBI)
 
 setwd("//Users/yawenliang/Documents/PadMonster")
 
 conn <- dbConnect(drv = RSQLite::SQLite(), "db/padmonster.sqlite3")
-Monster.db <- dbReadTable(conn,"Monster")
+Monster.db <- dbReadTable(conn, "Monster")
 Monster.dt <- data.table(Monster.db)
 MonsterIcon.dt <- Monster.dt[ , c("MonsterId", "MonsterIconPathDownload")]
 for (i in 1:nrow(MonsterIcon.dt)){
-  download.file(MonsterIcon.dt$MonsterIconPathDownload[i],
-                paste0("app/img/MonsterIcon/", MonsterIcon.dt[i,MonsterId], ".png"))
+  if (file.exists(paste0("app/img/MonsterIcon/", MonsterIcon.dt[i, MonsterId], ".png")) == FALSE) {
+      download.file(MonsterIcon.dt$MonsterIconPathDownload[i],
+                    paste0("app/img/MonsterIcon/", MonsterIcon.dt[i, MonsterId], ".png"))
+  }
 }
