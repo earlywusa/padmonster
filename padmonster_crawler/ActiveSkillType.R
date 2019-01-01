3124da18cfbc45474268b2922e9af3e92da35646
Yawen Liang <neway.liang@gmail.com>
2018-12-31 19:16:29 -0500

    Added ActiveSkillType.

--- padmonster_crawler/ActiveSkillType.R
+++ padmonster_crawler/ActiveSkillType.R
@@ -13,8 +13,8 @@ webpage <- read_html(url)
 webnodes1 <- html_nodes(webpage, 'h3')
 webnodes2 <- html_nodes(webpage, 'table')
 
-ActiveSkillType.ls <- as.list(html_text(webnodes1))
-ActiveSkillType.dt <- data.table(ActiveSkillType.ls)
+ActiveSkillTable.ls <- as.list(html_text(webnodes1))
+ActiveSkillTable.dt <- data.table(ActiveSkillTable.ls)
 ActiveSkillContent.ls_ <- lapply(webnodes2, function(x) html_text(html_nodes(x, "tr td")))
 ActiveSkillContent.ls_ <- ActiveSkillContent.ls_[4:(length(ActiveSkillContent.ls_ )-1)]
 
@@ -30,14 +30,38 @@ ActiveSkillContent.ls <- lapply(ActiveSkillContent.ls_, wide)
 
 
 ActiveSkillType.ls <- list()
-for (i in 1:length(ActiveSkillType.ls)){
-  ActiveSkillType.ls[[i]] <- ActiveSkillContent.ls[[i]][, ActiveSkillType := ActiveSkillType.ls[[i]]]
+for (i in 1:length(ActiveSkillTable.ls)){
+  ActiveSkillType.ls[[i]] <- ActiveSkillContent.ls[[i]][, ActiveSkillTable := ActiveSkillTable.ls[[i]] ]
 }
 
 ActiveSkillType.dt1 <- rbindlist(ActiveSkillType.ls)
+ActiveSkillType.dt2 <- ActiveSkillType.dt1[, ActiveSkillType := strsplit(ActiveSkillTable, "；", fixed=TRUE)]
+MaxComb <- max(sapply(ActiveSkillType.dt2$ActiveSkillType, length))
+ActiveSkillType.dt3 <- ActiveSkillType.dt1[, paste0("ActiveSkillType", 1:MaxComb) := tstrsplit(ActiveSkillTable, "；", fixed=TRUE)]
+ActiveSkillType.dt3[, c("ActiveSkillTable", "ActiveSkillType") := NULL]
 
+ActiveSkillType.dt4 <- melt(ActiveSkillType.dt3, id.vars = c("ActiveSkillName", "ActiveSkillDescription"),
+                           measure.vars = paste0("ActiveSkillType", 1:MaxComb), na.rm = TRUE)
+ActiveSkillType.dt4 [, c("variable", "ActiveSkillDescription") := NULL]
 
 # Get ActiveSkillId
-
 conn <- dbConnect(drv = RSQLite::SQLite(), "db/padmonster.sqlite3")
-miao <- dbReadTable(conn,"ActiveSkill")
+ActiveSkill <- dbReadTable(conn,"ActiveSkill")
+ActiveSkill.dt <- data.table(ActiveSkill)
+NameID <- ActiveSkill.dt[, .(ActiveSkillId, ActiveSkillName)]
+
+setkey(ActiveSkillType.dt4, ActiveSkillName)
+setkey(NameID, ActiveSkillName)
+ActiveSkillType.dt <- merge(NameID, ActiveSkillType.dt4, all.x = TRUE)
+
+ActiveSkillType.dt <- ActiveSkillType.dt[order(ActiveSkillId)]
+setnames(ActiveSkillType.dt, "value", "ActiveSkillType")
+ActiveSkillType.dt[, Id := 1:nrow(ActiveSkillType.dt)]
+
+setcolorder(ActiveSkillType.dt, c("Id", "ActiveSkillId", "ActiveSkillType", "ActiveSkillName"))
+
+
+# Write the data into database
+
+dbWriteTable(conn, "ActiveSkillType", ActiveSkillType.dt, overwrite = TRUE)
+dbDisconnect(conn) 
