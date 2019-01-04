6542a2eaf5e3cd45261ef26f88b96534c99a8045
Jutong Pan <jutong.pan@analysisgroup.com>
2019-01-04 10:29:11 -0500

    app.R: put the JS code for timeout workaround in the right place

--- app/app.R
+++ app/app.R
@@ -37,19 +37,6 @@ ui <- fluidPage (
         font-size: 10px;
       }
 
-      <script>
-      var socket_timeout_interval
-      var n = 0
-      $(document).on('shiny:connected', function(event) {
-        socket_timeout_interval = setInterval(function() {
-          Shiny.onInputChange('count', n++)
-        }, 15000)
-      });
-      $(document).on('shiny:disconnected', function(event) {
-        clearInterval(socket_timeout_interval)
-      });
-      </script>
-
     "))
   ),
 
@@ -61,6 +48,19 @@ ui <- fluidPage (
       }
     );'
   ),
+
+  tags$script("
+    var socket_timeout_interval
+    var n = 0
+    $(document).on('shiny:connected', function(event) {
+      socket_timeout_interval = setInterval(function() {
+        Shiny.onInputChange('count', n++)
+      }, 15000)
+    });
+    $(document).on('shiny:disconnected', function(event) {
+      clearInterval(socket_timeout_interval)
+    });
+  "),
 
   titlePanel("PAD Monsters"),
 
