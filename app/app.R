f11634f71bd309804b451553766172e1da0ba3e1
Jutong Pan <jutong.pan@analysisgroup.com>
2019-01-04 09:59:14 -0500

    app.R: add JS code to send data back and forth through websocket every x < 55 seconds to circumvent Heroku 55s timeout and keep the app alive

--- app/app.R
+++ app/app.R
@@ -37,6 +37,19 @@ ui <- fluidPage (
         font-size: 10px;
       }
 
+      <script>
+      var socket_timeout_interval
+      var n = 0
+      $(document).on('shiny:connected', function(event) {
+        socket_timeout_interval = setInterval(function() {
+          Shiny.onInputChange('count', n++)
+        }, 15000)
+      });
+      $(document).on('shiny:disconnected', function(event) {
+        clearInterval(socket_timeout_interval)
+      });
+      </script>
+
     "))
   ),
 
